6bd3ddd96172b37397db34a3e7cab7f9eb669488
foudm <foudmohamed2@gmail.com>
2024-10-09 01:29:32 +0200

    add Screenshots

--- app/src/main/java/com/example/project/network/URL.kt
+++ app/src/main/java/com/example/project/network/URL.kt
@@ -2,6 +2,6 @@ package com.example.project.network
 
 object URL {
 
-    const val BaseUrlApi = "http://192.168.31.55:8000/api/"
-    const val ImageUrl = "http://192.168.31.55:8000/"
+    const val BaseUrlApi = "http://192.168.100.114:8000/api/"
+    const val ImageUrl = "http://192.168.100.114:8000/"
 }
